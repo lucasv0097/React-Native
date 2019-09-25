52911cbe6e856e90310aa41ec1ec5eb43402aa9d
lucasv0097 <lucas_vieira97@hotmail.com>
2019-09-25 15:33:05 -0300

    working

--- insta_alura/App.js
+++ insta_alura/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import {
   StyleSheet,
@@ -8,49 +8,46 @@ import {
   Dimensions,
   ScrollView,
   FlatList
-} from 'react-native';
-import Post from './src/components/Post';
+} from "react-native";
+import Post from "./src/components/Post";
 
-const width = Dimensions.get('screen').width;
-const height = Dimensions.get('screen').height;
+const width = Dimensions.get("screen").width;
+const height = Dimensions.get("screen").height;
 
 export default class App extends React.Component {
   render() {
-
     const photo = [
-      { id: 1, user: 'Lucas' },
-      { id: 2, user: 'Daniela' },
-      { id: 3, user: 'Paulo' },
-      { id: 4, user: 'Luara' }
+      { id: 1, user: "Lucas" },
+      { id: 2, user: "Daniela" },
+      { id: 3, user: "Paulo" },
+      { id: 4, user: "Luara" }
     ];
     return (
-      <FlatList style={styles.container}
+      <FlatList
+        style={styles.container}
         keyExtractor={item => String(item.id)}
         data={photo}
-        renderItem={ ({item}) =>
-
-<Post foto={item}/>
-        }
+        renderItem={({ item }) => <Post foto={item} />}
       />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    marginTop:20
+  container: {
+    marginTop: 20
   },
-  header:{
+  header: {
     margin: 10,
-    flexDirection: 'row',
-    alignItems: 'baseline'
+    flexDirection: "row",
+    alignItems: "baseline"
   },
-  perfil_foto:{
+  perfil_foto: {
     width: 40,
     height: 40,
-    borderRadius: 20 
+    borderRadius: 20
   },
-  post:{
+  post: {
     marginRight: 10,
     width: width,
     height: width
